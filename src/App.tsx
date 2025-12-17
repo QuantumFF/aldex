74e48f017ad776f956a001cc6157fb73b752c216
QuantumFF <jonathanczt@gmail.com>
2025-12-17 17:31:52 +0800

    init shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { ComponentExample } from "@/components/component-example";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <div className="flex mb-8">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="w-24 h-24" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-5xl font-bold mb-4">Vite + React</h1>
-      <div className="p-4 border border-gray-700 rounded-lg">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p className="mt-4">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="mt-8 text-gray-500">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+export function App() {
+return <ComponentExample />;
 }
 
 export default App;
