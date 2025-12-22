05df45d0895e7aa892d4e53c56dc368fbeb29e8f
QuantumFF <jonathanczt@gmail.com>
2025-12-23 01:34:32 +0800

    fix: safely removed redundant src/app/dashboard/page.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Page from "@/app/dashboard/page";
+import { AlbumLibrary } from "@/components/album-library";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
 export function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Page />
+      <div className="flex flex-1 flex-col gap-4 p-8">
+        <AlbumLibrary />
+      </div>
       <Toaster />
     </ThemeProvider>
   );
--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AlbumLibrary } from "@/components/album-library";
-
-export default function Page() {
-  return (
-    <div className="flex flex-1 flex-col gap-4 p-8">
-      <AlbumLibrary />
-    </div>
-  );
-}
